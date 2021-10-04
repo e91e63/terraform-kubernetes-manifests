5f61af2c67bda5c53051f3e71bdc9be8b357ab45
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-10-03 20:56:34 -0700

    Updated deployment to ignore repeated changes

--- modules/alpine/main.tf
+++ modules/alpine/main.tf
@@ -1,4 +1,10 @@
 resource "kubernetes_pod" "alpine" {
+  lifecycle {
+    ignore_changes = [
+      spec.0.container.0.volume_mount
+    ]
+  }
+
   metadata {
     name      = var.alpine_conf.name
     namespace = "default"
