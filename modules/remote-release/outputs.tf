f33411638367151b4c51cbac268376c10942d864
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-05 23:28:38 -0700

    Updated outputs

--- modules/remote-release/outputs.tf
+++ modules/remote-release/outputs.tf
@@ -1,3 +1,8 @@
-output "route" {
-  value = var.route_conf.active ? module.traefik_ingress_route[0].route : "none"
+output "info" {
+  # sensitive = true
+  value = {
+    namespace = yamldecode(kubectl_manifest.namespace.yaml_body_parsed).metadata.name
+    route     = var.route_conf.active ? module.traefik_ingress_route[0].route : "none"
+
+  }
 }
