fdf290a1b1fc8841aef8435a39493dbad936eafc
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-11 14:23:07 -0800

    Fixing typo

--- modules/traefik-ingress-route/outputs.tf
+++ modules/traefik-ingress-route/outputs.tf
@@ -1,6 +1,6 @@
 locals {
   route_split = split("`", kubernetes_manifest.main.object.spec.routes[0].match)
-  subdomain   = local.route_split[1]
+  domain      = local.route_split[1]
   path        = local.route_split[3]
 }
 
