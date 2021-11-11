d44f0954cc0fb50fb0fb5ff94b98d4f4da4bbe26
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-11 14:19:28 -0800

    Updated traefik route outputs with path

--- modules/remote-release/outputs.tf
+++ modules/remote-release/outputs.tf
@@ -2,7 +2,7 @@ output "info" {
   # sensitive = true
   value = {
     namespace = yamldecode(kubectl_manifest.namespace.yaml_body_parsed).metadata.name
-    route     = var.route_conf.active ? module.traefik_ingress_route[0].route : "none"
+    route     = var.route_conf.active ? module.traefik_ingress_route[0].info.route : "none"
 
   }
 }
--- modules/traefik-ingress-route/outputs.tf
+++ modules/traefik-ingress-route/outputs.tf
@@ -1,3 +1,11 @@
-output "route" {
-  value = "https://${trimsuffix(trimprefix(kubernetes_manifest.main.object.spec.routes[0].match, "Host(`"), "`)")}/"
+locals {
+  route_split = split("`", kubernetes_manifest.main.object.spec.routes[0].match)
+  subdomain   = route_split[1]
+  path        = route_split[3]
+}
+
+output "info" {
+  value = {
+    route = "https://${local.domain}/${local.path}"
+  }
 }
