d5c65824e812abfd849e9e5c611f20ecbf206316
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-18 17:45:43 -0800

    Added null resource

--- modules/traefik/middlewares/null/main.tf
+++ modules/traefik/middlewares/null/main.tf
@@ -1,4 +1,11 @@
-
 terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+  }
   required_version = "~> 1"
 }
+
+resource "null_resource" "main" {}
